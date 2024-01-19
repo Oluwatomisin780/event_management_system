c27803f750f169cfd02a43c730ac3b201d816539
oluwatomisin780 <tomisinoyediran@gmail.com>
2024-01-19 14:09:36 +0100

    event_management

--- src/events/dto/update-event.input.ts
+++ src/events/dto/update-event.input.ts
@@ -3,7 +3,7 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateEventInput extends PartialType(CreateEventInput) {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
+  @Field(() => Int)
   id: number;
   @Field()
   name?: string;
